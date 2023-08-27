d1992758e74f6fbaad73deea942983a290dfb8b1
GolbanEF <golban.ef@yandex.ru>
2023-08-27 22:19:56 +0300

    go

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,16 +1,28 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 
 
+class MyPaginator(PageNumberPagination):
+    page_size = 2
+
+
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ["title", "description"]
+    # pagination_class = MyPaginator
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ["products"]
+    search_fields = ["positions__product__title"]
+    pagination_class = MyPaginator
